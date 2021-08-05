cbcca2b2f3f5874a76df093ba798be286d545619
Manojna6 <72754918+Manojna6@users.noreply.github.com>
2021-08-05 13:48:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+Webcam.attach('#webcam');
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("Snapshotdiv").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>';
+    });
+}
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/lA9---Id9/model.json', modelLoaded);
+function modelLoaded(){
+    console.log("model loaded");
+}
+function check() {
+    console.log('check');
+    img=document.getElementById("capture_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult() {
+    
+}
